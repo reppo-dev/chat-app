3ee9897d6a0f3a9537147f64f40aa60333fb3b82
soyr_6o <soyrozero@gmail.com>
2026-08-25 11:28:39 +0330

    feat(backend):add hash and check hash password

---
+++ backend/internal/utils/hashpassword.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hash ,err := bcrypt.GenerateFromPassword([]byte(password),bcrypt.DefaultCost)
+
+	if err != nil {
+		return "",err
+	}
+
+	return string(hash),nil
+}
+
+func CheckPasswordHash(hashpassword string,password string) error {
+	return  bcrypt.CompareHashAndPassword([]byte(hashpassword),[]byte(password))
+}
