779062249df59b59751d62634232e6d6e63066af
soyr_6o <soyrozero@gmail.com>
2026-08-29 12:01:06 +0330

    feat(backend):add func send event to all user and get all conections user

--- backend/internal/realtime/hub.go
+++ backend/internal/realtime/hub.go
@@ -1,2 +1,51 @@
 package realtime
 
+import (
+	"log"
+	"sync"
+
+	db "github.com/reppo-dev/chat-app/internal/db/sqlc"
+)
+
+type Hub struct {
+	Clients map[int64]map[*Client]struct{}
+	mu      sync.RWMutex
+	queries *db.Queries
+}
+
+func NewHub(queries *db.Queries) *Hub {
+	return &Hub{
+		queries: queries,
+	}
+}
+
+func (h *Hub) broadcastToAll(event Event) {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+	for _, conns := range h.Clients{
+		for c := range conns {
+			select{
+			case c.Send <- event:
+			default:
+				log.Printf("waring: dropped event for client %d, channel full",c.User.ID)
+			}
+		}
+	}
+}
+
+func (h *Hub) GetClients(userId int64) ([]*Client,bool) {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+
+	conns , ok := h.Clients[userId]
+	if !ok || len(conns) == 0 {
+		return nil,false
+	}
+
+	clients := make([]*Client,0,len(conns))
+	for c := range conns{
+		clients = append(clients,c)
+	}
+
+	return clients,true
+}
