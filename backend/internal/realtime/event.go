4c36adca1a3dfca8f386fb821076816e17ee2a69
soyr_6o <soyrozero@gmail.com>
2026-08-29 11:33:13 +0330

    feat(backend):add event type

---
+++ backend/internal/realtime/event.go
@@ -0,0 +1,22 @@
+package realtime
+
+type EventType string
+
+const (
+	EventCurrentUsers   EventType = "current_users"
+	EventUserOnline     EventType = "online"
+	EventUserOffline    EventType = "offline"
+	EventNewPrivate     EventType = "new_private"
+	EventMessage        EventType = "message"
+	EventDelivered      EventType = "delivered"
+	EventRead           EventType = "read"
+	EventTyping         EventType = "typing"
+	EventError          EventType = "error"
+	EventHeartbeat      EventType = "heartbeat"
+	EventServerShutdown EventType = "shutdown"
+)
+
+type Event struct {
+	EventType string `json:"event_type"`
+	Payload   any    `json:"payload"`
+}
